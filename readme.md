2a4a8f330ca1a95c72fa63a0f556f6e729b36c4a
rjRenolo <rjrenolo14@gmail.com>
2020-12-04 16:26:56 +0800

    distinct thingy

--- readme.md
+++ readme.md
@@ -4,3 +4,17 @@
 ### ?region=someregion&province=someprovince&municity=somemunicity
 
 or any combination of query parameters
+
+
+
+#### api/v1/hatdog/regions
+fetch all regions
+
+#### api/v1/hatdog/provinces
+### ?region=someregionname
+fetch all provinces on that region
+
+#### api/v1/hatdog/municities
+### ?province=someprovincename
+fetch all municipality/city on that province
+
--- routes/hangin.js
+++ routes/hangin.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
-
+const sequelize = require('sequelize')
 
 
 router.get("/all", (req, res) => {
@@ -25,6 +25,57 @@ router.get("/hamburger", (req, res) => {
     if(municity) halaman.where.in_municity = municity.toUpperCase()
 
     db.Somewhere.findAll(halaman)
+        .then((obj) => {
+            return res.status(200).json({ data: obj });
+        })
+        .catch((err) => {
+            console.log(err);
+            return res
+                .status(500)
+                .json({ msg: "Something doesn't seems right.", data: err });
+        });
+});
+
+router.get("/regions", (req, res) => {
+    // var halaman = {where: {}}
+    // const {region} = req.query
+    // if(region) halaman.where.in_region = region.toUpperCase()
+
+    db.Somewhere.findAll({ attributes: [[sequelize.fn('DISTINCT', sequelize.col('in_region')), 'region']]})
+        .then((obj) => {
+            return res.status(200).json({ data: obj });
+        })
+        .catch((err) => {
+            console.log(err);
+            return res
+                .status(500)
+                .json({ msg: "Something doesn't seems right.", data: err });
+        });
+});
+
+router.get("/provinces", (req, res) => {
+    var halaman = {where: {}}
+    const {region} = req.query
+    if(region) halaman.where.in_region = region.toUpperCase()
+
+    db.Somewhere.findAll({...halaman, attributes: [[sequelize.fn('DISTINCT', sequelize.col('in_province')), 'province']]})
+        .then((obj) => {
+            return res.status(200).json({ data: obj });
+        })
+        .catch((err) => {
+            console.log(err);
+            return res
+                .status(500)
+                .json({ msg: "Something doesn't seems right.", data: err });
+        });
+});
+router.get("/municities", (req, res) => {
+    var halaman = {where: {}}
+    const {region, province} = req.query
+    if(region) halaman.where.in_region = region.toUpperCase()
+    if(province) halaman.where.in_province = province.toUpperCase()
+
+    db.Somewhere.findAll({...halaman, attributes: [[sequelize.fn('DISTINCT', sequelize.col('in_municity')), 'municity']]})
         .then((obj) => {
             return res.status(200).json({ data: obj });
         })
